22eadf4eca36bc5ae02a07ab89ae0f2932f49494
Esperanza <erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>
2021-10-27 08:22:06 +0200

    Commit Inicial 2

--- js/eventos.js
+++ js/eventos.js
@@ -31,37 +31,41 @@ function manejadorClick2(){
   console.log("TROM2");
 
 }*/
-
+/*Función para que una misma seta se mueva, según donde hacemos click*/
 function ponerSeta(evento){
   //console.log("poner seta");
   console.log(evento);
   let seta = document.getElementById('setaPrueba');
+  //Definimos movimiento de la seta
   seta.style.top = `${evento.clientY -50 }px`;
   seta.style.left = `${evento.clientX -50 }px`;
 
 }
+/* Función para poner diferentes setas por el mapa, donde hagamos click*/
 function ponerSetas(evento){
   //console.log("poner setas");
-  console.log(evento);
+  //console.log(evento);
   let seta = document.createElement('img');  //Creamos img de seta
   document.body.appendChild(seta); //Lo añadimos a su padre
   seta.setAttribute('src','img/seta.png');  //Le agregamos atributos a img de seta indicando la imagen a visualizar
-  seta.setAttribute('class','setasPrueba');   //Le agregamos atributos a img de seta indicando la imagen a visualizar
+  seta.classList.add('setasPrueba');   //Le agregamos la clase correspondiente a img de seta
 
+  seta.oncontextmenu=quitarSetas; //Asociamos el evento a cada seta àra después poder quitarla
 
+  //Definimos posicionamiento de las setas
   seta.style.top = `${evento.clientY -50 }px`;
   seta.style.left = `${evento.clientX -50 }px`;
 
-  if(seta.oncontextmenu){
-    quitarSetas(seta);
-  }
-
 }
+/*Función para quitar las diferentes setas añadidas al mapa anteriormente, con click derecho*/
 function quitarSetas(evento){
   //console.log("quitar setas");
-  console.log(evento);
-  let seta = document.getElementsByTagName('p')[3];  //Buscamos el p que queremos destruir
-  seta.remove(); //Borramos seta
+  //console.log(evento);
+  //Quitamos el menú del navegador que nos aparece al hacer clik derecho
+  evento.stopPropagation();
+  evento.preventDefault();
 
+  //Borrado de seta, target indica que imagen es la que esta entrendo con el evento, al borrar target borramos imagen
+  evento.target.remove();
 
 }
